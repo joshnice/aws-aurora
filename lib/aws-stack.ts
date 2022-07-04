748af82cd26f94e8991f18b39094f74eec02f063
joshnice <joshliamnice@gmail.com>
2022-07-04 15:50:28 +0100

    Added a vpc and then an aurora database

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,16 +1,26 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_ec2 as ec2, aws_rds as rds } from "aws-cdk-lib";
 
 export class AwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "MyFirstBucket", {
-      versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      bucketName: "josh-nice-cdk-bucket",
+    const vpc = new ec2.Vpc(this, "Aurora-vpc");
+
+    const aurora = new rds.ServerlessCluster(this, "Aurora-db-cluster", {
+      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      vpc,
+      parameterGroup: rds.ParameterGroup.fromParameterGroupName(
+        this,
+        "parameter-group",
+        "default.aurora-postgresql10"
+      ),
+      defaultDatabaseName: "MyDb",
+      scaling: {
+        maxCapacity: rds.AuroraCapacityUnit.ACU_2,
+        minCapacity: rds.AuroraCapacityUnit.ACU_2,
+      },
     });
   }
 }
